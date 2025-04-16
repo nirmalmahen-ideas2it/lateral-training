2e5d10b47e54887621544640892b28273d8e7e53
nirmalmahen-ideas2it <alagunirmal.mahendran@ideas2it.com>
2025-04-16 13:59:51 +0530

    Added configs related to spring cloud config

--- myFirst-microservice/src/main/java/com/myfirst/microservice/config/ModelMapperConfig.java
+++ myFirst-microservice/src/main/java/com/myfirst/microservice/config/ModelMapperConfig.java
@@ -1,8 +1,10 @@
 package com.myfirst.microservice.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class ModelMapperConfig {
@@ -10,4 +12,14 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    CommandLineRunner showConfig(Environment env) {
+        return args -> {
+            System.out.println("----- Loaded properties -----");
+            System.out.println("server.port = " + env.getProperty("server.port"));
+            System.out.println("Database URL = " + env.getProperty("spring.cloud.datasource.url"));
+        };
+    }
+
 }
